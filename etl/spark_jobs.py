3c92f6c665b0e561175cab3304f505346908833e
Caio41 <caiaoviniciuscc@gmail.com>
2026-03-14 17:14:12 -0300

    refactor: separates spark transformation logic into transform.py

--- etl/spark_jobs.py
+++ etl/spark_jobs.py
@@ -1,50 +1,28 @@
 import sys
 
 from pyspark.sql import SparkSession 
-from pyspark.sql.functions import col, substring
+
+from etl.transform import transform_match_dim, transform_player_matches, transform_team_matches
 
 
 def run_spark_etl(raw_path: str, processed_data_path: str):
+    print('Reading data...')
     spark = SparkSession.builder.appName('esports_etl').getOrCreate()
-
 
     raw_df = spark.read.csv(raw_path, header=True)
 
-    players_df = raw_df.filter(col('position') != 'team')
-    teams_df = raw_df.filter(col('position') == 'team')
+    print('Processing data...')
+    fact_player_matches = transform_player_matches(raw_df)
+    fact_team_matches = transform_team_matches(raw_df)
+    dim_match = transform_match_dim(raw_df) 
 
-    fact_player_matches = players_df.select(
-        'gameid', 'url', 'league', 'side', 'position', 'playername',
-        'teamname', 'champion', 
-        col('kills').cast('int'), 
-        col('deaths').cast('int'), 
-        col('assists').cast('int'), 
-        col('result').cast('int')
-    )
-
-    fact_team_matches = teams_df.select(
-        'gameid', 'league', 'teamname', 'side', 
-        col('kills').cast('int'), 
-        col('dragons').cast('int'), 
-        col('barons').cast('int'), 
-        col('result').cast('int')
-    )
-
-
-    dim_match = raw_df.select(
-        'gameid', 'league', 'date', 'patch', 
-        (
-            substring(col('gamelength'), 1, 2).cast('int') * 60 + 
-            substring(col('gamelength'), 3, 2).cast('int')
-        ).alias('gamelength_seconds')
-    ).distinct()
-
-
+    print('Saving data...')
     fact_player_matches.write.mode('overwrite').parquet(f'{processed_data_path}/fact_player_matches.parquet')
     fact_team_matches.write.mode('overwrite').parquet(f'{processed_data_path}/fact_team_matches.parquet')
     dim_match.write.mode('overwrite').parquet(f'{processed_data_path}/dim_match.parquet')
 
     spark.stop()
+    print('Job finished')
 
 
 
--- etl/transform.py
+++ etl/transform.py
@@ -1,54 +1,44 @@
-import pandas as pd 
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col, substring
 
-raw_df = pd.read_csv('data/raw/2026_LoL_esports_match_data_from_OraclesElixir.csv')
+def transform_player_matches(raw_df: DataFrame) -> DataFrame:
+    players_df = raw_df.filter(col('position') != 'team')
+    
+    fact_player_matches = players_df.select(
+        'gameid', 'url', 'league', 'side', 'position', 'playername',
+        'teamname', 'champion', 
+        col('kills').cast('int'), 
+        col('deaths').cast('int'), 
+        col('assists').cast('int'), 
+        col('result').cast('int')
+    )
 
-players_df = raw_df[raw_df['position'] != 'team']
-teams_df = raw_df[raw_df['position'] == 'team']
-
-print(list(raw_df.columns))
-
-
-fact_player_matches = players_df[
-    [
-        'gameid',
-        'url',
-        'league',
-        'side',
-        'position',
-        'playername',
-        'teamname',
-        'champion',
-        'kills',
-        'deaths',
-        'assists',
-        'result'
-    ]
-]
-
-fact_team_matches = teams_df[
-    [
-        'gameid',
-        'league',
-        'teamname',
-        'side',
-        'kills',
-        'dragons',
-        'barons',
-        'result'
-    ]
-]
-
-dim_match = raw_df[
-    [
-        'gameid',
-        'league',
-        'date',
-        'patch',
-        'gamelength'
-    ]
-].drop_duplicates()
+    return fact_player_matches
 
 
-fact_player_matches.to_parquet('data/processed/fact_player_matches.parquet')
-fact_team_matches.to_parquet('data/processed/fact_team_matches.parquet')
-dim_match.to_parquet('data/processed/dim_match.parquet')
+def transform_team_matches(raw_df: DataFrame) -> DataFrame:
+    teams_df = raw_df.filter(col('position') == 'team')
+
+    fact_team_matches = teams_df.select(
+        'gameid', 'league', 'teamname', 'side', 
+        col('kills').cast('int'), 
+        col('dragons').cast('int'), 
+        col('barons').cast('int'), 
+        col('result').cast('int')
+    )
+    
+    return fact_team_matches
+
+
+def transform_match_dim(raw_df: DataFrame) -> DataFrame:
+    
+    dim_match = raw_df.select(
+        'gameid', 'league', 'date', 'patch', 
+        (
+            substring(col('gamelength'), 1, 2).cast('int') * 60 + 
+            substring(col('gamelength'), 3, 2).cast('int')
+        ).alias('gamelength_seconds')
+    ).distinct()
+
+    return dim_match
+
